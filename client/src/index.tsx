5876c4a9e0021957db2e3c2eb656428f1bf81842
Fahad Khalid <fahadkhalid695@gmail.com>
2025-09-08 20:22:26 +0500

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { reportWebVitals } from './reportWebVitals';
+import { reportWebVitals } from './reportWebVitals.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
